77b6888cef5db7e2217364786bcfca2261d83f83
Julien Morelle <julien.morelle@etu.univ-grenoble-alpes.fr>
2022-02-10 21:56:00 +0100

    images

--- code/particles.py
+++ code/particles.py
@@ -43,6 +43,10 @@ class AnimationPlayer:
         animation_frames = choice(self.frames['grass'])
         ParticleEffect(pos, animation_frames, groups)
 
+    def create_particles(self, animation_type, pos, groups):
+        animation_frames = self.frames[animation_type]
+        ParticleEffect(pos, animation_frames, groups)
+
 
 class ParticleEffect(pygame.sprite.Sprite):
     def __init__(self, pos, animation_frames, groups):
